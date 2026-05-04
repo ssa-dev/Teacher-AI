ef11bf49ee5c4ba438385f4577bb140f2e2b57b8
Suliman <suliman.dev.sa@gmail.com>
2026-05-04 12:41:55 +0300

    fix port

--- server.js
+++ server.js
@@ -121,6 +121,9 @@ mindmap-leaf
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 });
